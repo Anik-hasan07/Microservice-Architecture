007276e10d55e681a8b9277bb2c4169d604f1d29
Anik-hasan07 <iamakashcse@gmail.com>
2024-03-31 18:58:30 +0600

    Monorepo setup

---
+++ libs/common/src/database/abstract.schema.ts
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+import { Prop, Schema } from '@nestjs/mongoose';
+import { SchemaTypes, Types } from 'mongoose';
+
+@Schema()
+export class AbstractDocument {
+  @Prop({ type: SchemaTypes.ObjectId })
+  _id: Types.ObjectId;
+}
